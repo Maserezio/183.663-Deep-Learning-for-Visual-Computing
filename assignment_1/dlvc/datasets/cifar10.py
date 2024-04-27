24bc2313ea7fe74043585cd337a1869ebe84a4d2
artur_sog <e12143554@student.tuwien.ac.at>
2024-04-27 20:10:45 +0200

    data loading done

--- assignment_1/dlvc/datasets/cifar10.py
+++ assignment_1/dlvc/datasets/cifar10.py
@@ -1,9 +1,11 @@
+import os
 import pickle
 from typing import Tuple
 import numpy as np
 
+# from dlvc.datasets.dataset import  Subset, ClassificationDataset
+from .dataset import Subset, ClassificationDataset
 
-from dlvc.datasets.dataset import  Subset, ClassificationDataset
 
 class CIFAR10Dataset(ClassificationDataset):
     '''
@@ -26,18 +28,48 @@ class CIFAR10Dataset(ClassificationDataset):
         '''
 
         self.classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-        
+                        'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
         ## TODO implement
         # See the CIFAR-10 website on how to load the data files
-        pass
+        if not os.path.isdir(fdir):
+            raise ValueError(f"{fdir} is not a directory")
+
+        self.transform = transform
+        self.data = []
+        self.labels = []
+
+        if subset == Subset.TRAINING:
+            batch_files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4']
+        elif subset == Subset.VALIDATION:
+            batch_files = ['data_batch_5']
+        elif subset == Subset.TEST:
+            batch_files = ['test_batch']
+        else:
+            raise ValueError("Invalid subset")
+
+        for batch_file in batch_files:
+            file_path = os.path.join(fdir, batch_file)
+            if not os.path.isfile(file_path):
+                raise ValueError(f"File {file_path} is missing")
+
+            with open(file_path, 'rb') as file:
+                batch_data = pickle.load(file, encoding='latin1')  # or encoding='bytes'
+                self.data.append(batch_data['data'])
+                self.labels += batch_data['labels']
+
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32).transpose(0, 2, 3, 1)
+        self.labels = np.array(self.labels)
+
+        if self.data.shape[0] != len(self.labels):
+            raise ValueError("Number of images and labels do not match")
 
     def __len__(self) -> int:
         '''
         Returns the number of samples in the dataset.
         '''
         ## TODO implement
-        pass
+        return len(self.data)
 
     def __getitem__(self, idx: int) -> Tuple:
         '''
@@ -47,12 +79,18 @@ class CIFAR10Dataset(ClassificationDataset):
         Raises IndexError if the index is out of bounds.
         '''
         ## TODO implement
-        pass
+        if idx >= len(self.data):
+            raise IndexError("Index out of bounds")
+
+        img, label = self.data[idx], self.labels[idx]
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label
 
     def num_classes(self) -> int:
         '''
         Returns the number of classes.
         '''
         ## TODO implement
-        pass
-
+        return len(self.classes)
